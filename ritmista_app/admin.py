f91437d4d1dedfb3d3d9836e7d5838e79775b1cd
Victor Pecine <victorpecine@gmail.com>
2021-03-20 17:01:45 -0300

    Modelo do banco e criação do admin

--- ritmista_app/admin.py
+++ ritmista_app/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from ritmista_app.models import Curso, Grupo, Naipe, Ritmista
 
-# Register your models here.
+
+class Grupos(admin.ModelAdmin):
+    list_display = ('id', 'grupo')
+    list_display_links = ('id', 'grupo')
+    search_fields = ('grupo',)
+    list_filter = ('grupo',)
+
+admin.site.register(Grupo, Grupos)
+
+
+class Naipes(admin.ModelAdmin):
+    list_display = ('id', 'naipe')
+    list_display_links = ('id', 'naipe')
+    search_fields = ('naipe',)
+    list_filter = ('naipe',)
+
+admin.site.register(Naipe, Naipes)
+
+
+class Cursos(admin.ModelAdmin):
+    list_display = ('id', 'curso')
+    list_display_links = ('id', 'curso')
+    search_fields = ('curso',)
+    list_filter = ('curso',)
+
+admin.site.register(Curso, Cursos)
+
+
+class Ritmistas(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'curso', 'naipe', 'cpf', 'telefone')
+    list_display_links = ('nome', 'naipe')
+    search_fields = ('nome', 'naipe', 'curso')
+    list_filter = ('naipe', 'data_entrada',)
+
+admin.site.register(Ritmista, Ritmistas)
--- ritmista_app/models.py
+++ ritmista_app/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+
+class Grupo(models.Model):
+    grupo = models.CharField(max_length=10, blank=False, null=False, unique=True)
+
+    def __str__(self):
+        return self.grupo
+
+
+class Naipe(models.Model):
+    naipe = models.CharField(max_length=50, blank=False, null=False, unique=True)
+
+    def __str__(self):
+        return self.naipe
+
+
+class Curso(models.Model):
+    curso = models.CharField(max_length=100, blank=False, null=False, unique=True)
+
+    def __str__(self):
+        return self.curso
+
+
+class Ritmista(models.Model):
+    nome = models.CharField(max_length=200, blank=False, null=False, db_column='Nome')
+    curso = models.ForeignKey(Curso, on_delete=models.DO_NOTHING, default=None, db_column='Curso') 
+    naipe = models.ForeignKey(Naipe, on_delete=models.DO_NOTHING, default=None, db_column='Naipe')
+    grupo = models.ManyToManyField(Grupo, blank=False, db_column='Grupo', default=None)
+    data_entrada = models.DateField(blank=False, null=False, db_column='Data de entrada', verbose_name='Data de entrada')
+    data_nascimento = models.DateField(blank=False, null=False, db_column='Data de nascimento', verbose_name='Data de nascimento', default=None)
+    cpf = models.CharField(max_length=11, blank=False, null=False, db_column='CPF', verbose_name='CPF', unique=True, default=None)
+    telefone = models.CharField(max_length=11, blank=False, null=False, db_column='Telefone', default=None)
+    data_saida = models.DateField(blank=True, null=True, db_column='Data de saída', verbose_name='Data de saída')
+
+    def __str__(self):
+        return self.nome
